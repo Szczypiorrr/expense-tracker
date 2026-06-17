97a21f352ded52aaf2b6e48e3e81bec97b386fd1
Szczypiorrr <wintheer09@gmail.com>
2026-06-17 23:09:29 +0200

    fix : add missing model imports in init_db

--- database/init_db.py
+++ database/init_db.py
@@ -1,4 +1,8 @@
-from db import Base, engine
+from database.db import Base, engine
+
+from models.category import Category
+from models.expense import Expense
+from models.user import User
 
 def init_database():
     Base.metadata.create_all(engine)
