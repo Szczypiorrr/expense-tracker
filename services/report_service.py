6751716502a11a2055acb91160ebaf50e902db9d
Szczypiorrr <wintheer09@gmail.com>
2026-06-20 01:15:00 +0200

    feat: complete report service impelentation

--- services/report_service.py
+++ services/report_service.py
@@ -18,13 +18,13 @@ def show_monthly_summary(month, year):
 
     total_expenses = session.query(func.sum(Expense.amount)).scalar()
 
-    print(f"Total expenses: {total_expenses} PLN\n")
+    print(f"Total expenses: {round(total_expenses, 2)} PLN\n")
 
     print(f"Number of expenses: {len(expenses)}\n")
 
     avarge_expense = session.query(func.avg(Expense.amount)).scalar()
 
-    print(f"Avarge expense amount: {avarge_expense}\n")
+    print(f"Avarge expense amount: {round(avarge_expense, 2)} PLN\n")
 
     most_expensive = session.query(Expense).order_by(Expense.amount).first()
 
@@ -35,9 +35,11 @@ def show_monthly_summary(month, year):
     print(f"Expenses by category:\n")
     for category in categories:
         total = sum(expense.amount for expense in category.expenses)
-        print(f"{category.name} - {total}")
+        print(f"{category.name} - {round(total, 2)} PLN")
 
     session.close()
+
+
 
 
 def generate_report(username, date):
@@ -56,7 +58,48 @@ def generate_report(username, date):
 
         f.write("------------------------------------\n\n")
 
-        f.write("SUMMARY\n")
+        expenses = session.query(Expense).where(and_(Expense.user_id == user.id, Expense.date >= date, Expense.date <= now)).all()
+
+        f.write("SUMMARY:\n")
 
         total_expenses = session.query(func.sum(Expense.amount)).where(and_(Expense.user_id == user.id, Expense.date >= date, Expense.date <= now)).scalar()
-        f.write(f"Total expenses: {total_expenses} PLN\n")
+
+        f.write(f"Total expenses: {round(total_expenses, 2)} PLN\n")
+        f.write(f"Number of expenses: {len(expenses)}\n")
+
+        avg_expenses = session.query(func.avg(Expense.amount)).where(and_(Expense.user_id == user.id, Expense.date >= date, Expense.date <= now)).scalar()
+
+        f.write(f"Avarge expense: {round(avg_expenses, 2)} PLN\n\n")
+
+        highest_expense = session.query(Expense).where(Expense.user_id == user.id).order_by(Expense.amount.desc()).first()
+
+        f.write("HIGHEST EXPENSE:\n")
+        f.write(f"{highest_expense.description} - {round(highest_expense.amount, 2)} PLN ({highest_expense.category.name})\n\n")
+
+        lowest_expense = session.query(Expense).where(Expense.user_id == user.id).order_by(Expense.amount).first()
+
+        f.write("LOWEST EXPENSE:\n")
+        f.write(f"{lowest_expense.description} - {round(lowest_expense.amount, 2)} PLN ({lowest_expense.category.name})\n\n")
+
+        f.write("BY CATEGORY:\n")
+        category_totals = {}
+
+        for expense in expenses:
+            category_name = expense.category.name
+
+            if category_name not in category_totals:
+                category_totals[category_name] = 0
+
+            category_totals[category_name] += expense.amount
+
+        for name, total in category_totals.items():
+            f.write(f"{name} - {round(total, 2)} PLN\n")
+
+        f.write("\nTOP 5 EXPENSES:\n")
+        id = 1
+        for expense in expenses:
+            f.write(f"{id}. {expense.description} - {expense.amount} PLN\n")
+            id += 1
+        
+        session.close()
+
