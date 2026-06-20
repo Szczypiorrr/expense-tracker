2c1d94f638a1da174c660bac4900dfe1a918e55e
Szczypiorrr <wintheer09@gmail.com>
2026-06-20 13:52:03 +0200

    refactor: clean up unused service functions

--- services/expense_service.py
+++ services/expense_service.py
@@ -13,31 +13,6 @@ def create_expense(amount, description, date, user_id, category_id):
     
     return expense
 
-def delete_expense(expense_id):
-    session = Session()
-
-    expense = session.query(Expense).where(Expense.id == expense_id).first()
-
-    if not expense:
-        session.close()
-        return None
-
-    session.delete(expense)
-    session.commit()
-
-    session.close()
-
-    return
-
-def get_expense(expense_id):
-    session = Session()
-
-    expense = session.query(Expense).where(Expense.id == expense_id).first()
-
-    session.close()
-
-    return expense
-
 def get_all_expenses():
     session = Session()
 
